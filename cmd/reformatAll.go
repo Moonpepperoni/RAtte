ac69b9a26ead7bb061745659b5dc15f119dec3c8
Jan <jan_leonhardt@icloud.com>
2023-01-20 23:59:13 +0100

    remove functionality from reformat all reformat all is less flexible but easier to use now

--- cmd/reformatAll.go
+++ cmd/reformatAll.go
@@ -22,15 +22,13 @@ import (
 
 // reformatAllCmd represents the all subcommand to of the reformat command
 var reformatAllCmd = &cobra.Command{
-	Use:   "all [directory]",
-	Short: "Applies the reformat command to all files in the optionally specified directory.",
-	Long: `Applies the reformat command to all .sec files in the specified directory.
-directory is an optional argument. Calling reformat all without specifying directory explicitly will run reformat in the currently selected directory: e.g. ./  
+	Use:   "all",
+	Short: "Searches the current directory for .sec files recursively and applies the reformatting",
+	Long: `Searches the current directory for .sec files recursively and changes the file extension of all *asm.sec files to *.asm
 
 Examples:
-  ratte reformat all
-  ratte reformat all assignment1`,
-	Args: cobra.MaximumNArgs(1),
+  ratte reformat all`,
+	Args: cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("called all subcommand of reformat")
 	},
